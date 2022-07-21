96175a6afc22b45f6b0b6d53fc83f8cece54ddbc
SleepyStew <sleepystew3@gmail.com>
2022-07-21 21:11:54 +1000

    reminders

--- reminders/views.py
+++ reminders/views.py
@@ -24,7 +24,7 @@ def reminders(request):
         return render(request, 'reminders/reminders.html', context={'discordoauthed': discordoauthed})
     reminders = []
     for reminder in Reminder.objects.filter(owner=request.user).order_by('due'):
-        reminder.date = datetime.fromtimestamp(reminder.due).astimezone(pytz.timezone('Australia/Melbourne')).strftime('%H:%M %d/%m/%Y')
+        reminder.date = datetime.fromtimestamp(reminder.due, tz=pytz.timezone('aest')).strftime('%H:%M %d/%m/%Y')
         reminders.append(reminder)
     return render(request, 'reminders/reminders.html', context={'reminders': reminders, 'discordoauthed': discordoauthed, 'discord_full_name': discord_full_name})
 
