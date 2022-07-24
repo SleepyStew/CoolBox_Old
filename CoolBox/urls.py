dde12a3465718d7c6ce1d4d5b13e792276bbf55f
SleepyStew <sleepystew3@gmail.com>
2022-07-24 18:44:04 +1000

    updated url schema fix

--- CoolBox/urls.py
+++ CoolBox/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.shortcuts import redirect
-from django.urls import path, include
+from django.urls import path, include, reverse
 
 from . import root
 
